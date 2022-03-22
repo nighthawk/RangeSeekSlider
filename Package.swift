d21f6500003f561ff74d2295f0ee96ebafdab130
jhSeo <junghun@wantedlab.com>
2022-03-22 09:29:59 +0900

    fix: update package manager

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v11),
+        .tvOS(.v11)
+    ],
+    products: [
+        .library(name: "RangeSeekSlider", targets: ["RangeSeekSlider"])
+    ],
+    targets: [
+        .target(name: "RangeSeekSlider", path: "Sources")
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
 )
